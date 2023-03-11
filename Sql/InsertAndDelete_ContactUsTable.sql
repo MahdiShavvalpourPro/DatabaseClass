272eda0827fb39e7700fad9714b6d624f6e86543
Mahdi.Shavvalpoor <m.shavval@gmail.com>
2023-03-11 18:26:50 +0330

    Init

---
+++ Sql/InsertAndDelete_ContactUsTable.sql
@@ -0,0 +1,7 @@
+INSERT INTO contactus(FirstName,LastName,MobileNumber,Email,Title,Message) 
+VALUES ('Mohammad','Rezvani','09162356547','Test@gmail.com','خطا هنگام دانلود فایل','لطفا پیگیری شود',)
+
+////////////
+
+DELETE FROM contactus 
+WHERE Id=1
---
+++ Sql/InsertAndDelete_UserTable.sql
@@ -0,0 +1,24 @@
+INSERT INTO users (FirstName,LastName,Username,Password,Email,Status)
+VALUES ('Mahdi','Mansori','09456547894','f+fkghfg','Mnasir@gamil.com','active');
+
+INSERT INTO users (FirstName,LastName,Username,Password,Email,Status)
+VALUES ('Abolfazl','Mohammadi','09146247234','d6f5g4fg','abmahd@gamil.com','active');
+
+INSERT INTO users (FirstName,LastName,Username,Password,Email,Status)
+VALUES ('Abbas','Alami','09163256324','dafdvdv00d54f','absali@gamil.com','active');
+
+
+////////////////
+
+
+--DELETE From users
+--WHERE Status='ban';
+
+DELETE FROM users
+WHERE Id=1
+
+
+
+
+
+
